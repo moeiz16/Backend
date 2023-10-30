fe1bd79083b82ed5bd015b812d02d00d865a1644
Junaid Ali Bhatti <junaid.ali101452@gmail.com>
2023-10-31 04:02:30 +0500

    issues resolved in calculating the hetrogeneity value

--- backendapp/views.py
+++ backendapp/views.py
@@ -242,62 +242,85 @@ def polygon_point_wise_ndvi(request):
     else:
         return JsonResponse({'error': 'Invalid request method'})
 
+def ndvi_heterogeneity_helper_function(ndvi_values):
+    # Convert NDVI values to a NumPy array
+    ndvi_array = np.array(ndvi_values)
+    
+    # Calculate the standard deviation of NDVI values
+    ndvi_std_dev = np.std(ndvi_array)
+    
+    return ndvi_std_dev
+    
 def ndvi_heterogeneity(request):
     if request.method == 'POST':
         if check_gee_initialized():
             start_time = time.time()
             postData = json.loads(request.body)
-            polygon_list = postData['polygon']
+
+            polygonPoint = postData['polygon']
             start_date = postData['startDate']
             end_date = postData['endDate']
 
+            point_ndvi_computed_list = []
+            ndvi_values_list = []  # List to store NDVI values
+
+            polygon_vertices = polygonPoint['polygon']['coordinates']
+            points_list = polygonPoint['points']
+
+            # Convert polygon vertices to Earth Engine format
+            polygon_coords = [(vertex['lng'], vertex['lat'])
+                              for vertex in polygon_vertices]
+            polygon_geometry = ee.Geometry.Polygon([polygon_coords])
+
+            # Load Sentinel-2 imagery
             sentinel2 = ee.ImageCollection("COPERNICUS/S2_SR")
-            collection = sentinel2 \
-                .filterDate(start_date, end_date)
+            sentinel_collection = sentinel2 \
+                .filterDate(start_date, end_date) \
+                .filterBounds(polygon_geometry)
 
-            ndvi_heterogeneity_list = []
+            # Create an EE Geometry MultiPoint from the list of points
+            points_geometry = ee.Geometry.MultiPoint(
+                [ee.Geometry.Point(point['lng'], point['lat'])
+                    for point in points_list]
+            )
 
-            for polygon in polygon_list:
-                polygon_vertices = polygon['coordinates']
+            # Filter the image collection using the polygon
+            filtered_collection = sentinel_collection.filterBounds(
+                points_geometry)
 
-                # Convert the polygon vertices to Earth Engine format
-                polygon_coords = [(vertex['lng'], vertex['lat'])
-                                  for vertex in polygon_vertices]
-                polygon_geometry = ee.Geometry.Polygon([polygon_coords])
-                sentinel_collection = collection.filterBounds(polygon_geometry)
+            # Map the NDVI calculation function over the filtered collection
+            filtered_with_ndvi = filtered_collection.map(calculate_ndvi)
 
-                # Map the NDVI calculation function over the collection
-                sentinel_with_ndvi = sentinel_collection.map(calculate_ndvi)
+            # Select the NDVI band from the processed collection
+            ndvi_collection = filtered_with_ndvi.select('NDVI')
 
-                # Select the NDVI band from the processed collection
-                ndvi_collection = sentinel_with_ndvi.select('NDVI')
+            # Get NDVI values for each point
+            ndvi_values = ndvi_collection.getRegion(
+                points_geometry, scale=10).getInfo()
 
-                # Calculate NDVI values for the selected area and time frame
-                ndvi_values = ndvi_collection.reduceRegion(
-                    reducer=ee.Reducer.toList(),
-                    geometry=polygon_geometry,
-                    scale=10
-                )
+            # Extract the NDVI values from the response
+            header = ndvi_values[0]
+            data = ndvi_values[1:]
+            ndvi_index = header.index('NDVI')
 
-                # Extract NDVI values as a list
-                ndvi_list = ee.List(ndvi_values.get('NDVI'))
 
-                # Calculate NDVI heterogeneity (standard deviation)
-                ndvi_array = np.array(ndvi_list.getInfo())
-                ndvi_std = np.std(ndvi_array)
+            for point, row in zip(points_list, data):
+                ndvi = row[ndvi_index]
+                ndvi = math.ceil(ndvi * 1000) / 1000
+                ndvi_values_list.append(ndvi)  # Append NDVI value to the list
 
-                ndvi_heterogeneity_list.append({
-                    'polygonVertices': polygon_vertices,
-                    'ndviHeterogeneity': ndvi_std
-                })
+            # Calculate NDVI heterogeneity using the ndvi_heterogeneity_helper_function
+            ndvi_heterogeneity_value = ndvi_heterogeneity_helper_function(ndvi_values_list)
 
             end_time = time.time()
             elapsed_time = end_time - start_time
             print(f"Total time taken: {elapsed_time:.2f} seconds")
+            print("NDVI Hetrogeneity Value is: ",ndvi_heterogeneity_value)
 
-            return JsonResponse({'ndviHeterogeneityList': ndvi_heterogeneity_list})
+            return JsonResponse({'ndviHeterogeneity': ndvi_heterogeneity_value})
         else:
             initialize_gee()
             return JsonResponse({'message': 'EE is initialized now'})
+
     else:
         return JsonResponse({'error': 'Invalid request method'})
